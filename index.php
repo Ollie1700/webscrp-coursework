db10b870d8cfad12654c79e506d1397c67b356e7
Oliver Pennington <typlosion@hotmail.co.uk>
2016-02-11 14:14:37 +0000

    Preliminary.

--- index.php
+++ index.php
@@ -18,33 +18,51 @@
                     <span class="subtitle">Instant messaging for teams</span>
                 </h1>
             </div>
-            <div class="profile">
-                <img src="img/default_user_profile_pic.png">
-                <span class="name">
-                    <?php
-                    
-                        if(isset($CURRENT_USER)) {
-                            echo 'Welcome back, ' . $CURRENT_USER->get_user_first_name() . '!';
-                        }
-                        else {
-                            include 'login.php';
-                        }
-
-                    ?>
-                </span>
-                <span class="description">Design Team</span>
-            </div>
+            
+            <?php if(isset($CURRENT_USER)) : ?>
+                <div class="profile">
+                    <img src="img/default_user_profile_pic.png">
+                    <span class="name">
+                        <?php echo 'Welcome back, ' . $CURRENT_USER->get_user_first_name() . '!'; ?>
+                    </span>
+                    <span class="description">Design Team</span>
+                </div>
+            <?php else : ?>
+            <?php endif; ?>
         </header>
         
         <section id="channel-container" class="channel-container">
             <?php if(isset($CURRENT_USER)) : include 'api/rooms.php'; else : ?>
                 
-                
+                <div class="user-login-container">
+                    
+                    <h1>Welcome! Please log in or register.</h1>
+                    
+                    <div id="user-login-form-container">
+                        <?php include 'inc/login.php'; ?>
+                    </div>
+                    <div id="user-register-form-container">
+                        <?php include 'inc/register.php'; ?>
+                    </div>
+                    
+                </div>
                 
             <?php endif; ?>
         </section>
         
-        <script>
-        </script>
+        <script src="lib/user.js"></script>
     </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
